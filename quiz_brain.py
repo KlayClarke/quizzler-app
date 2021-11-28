049829f428d7a0f17efeb57f3d82a5038cc80a36
Klay Clarke <klayaclarke@gmail.com>
2021-11-28 12:34:35 -0500

    fix bug that would allow user to increase score by clicking the answer of the final question after the quiz had reached its limit of 10 questions
    if current question number is 10, set current question answer to 'None', so neither button can be fooled into increasing the score of our user

--- quiz_brain.py
+++ quiz_brain.py
@@ -22,11 +22,9 @@ class QuizBrain:
 
     def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
-        if user_answer.lower() == correct_answer.lower():
+        if self.question_number == 10:
+            self.current_question.answer = None
+        elif user_answer.lower() == correct_answer.lower():
             self.score += 1
-            print("You got it right!")
-        else:
-            print("That's wrong.")
-
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
+        print(f'Correct answer is {self.current_question.answer}')
+        return self.score
