9ee17f7c7b6e9ac1d5603b44e6c07434ea26a952
Klay Clarke <klayaclarke@gmail.com>
2021-11-28 04:48:28 -0500

    create tkinter window for quiz app

--- ui.py
+++ ui.py
@@ -1,2 +1,15 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+
+class QuizInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('Quizzler')
+        self.window.minsize(width=400, height=500)
+        self.window.config(padx=20,pady=20,bg=THEME_COLOR)
+        self.canvas = Canvas(width=300, height=250, highlightthickness=0)
+
+
+        self.window.mainloop()
