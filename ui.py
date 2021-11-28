0d3f4ee8e1ef2e312557a49bc4577d5dbe48a7a6
Klay Clarke <klayaclarke@gmail.com>
2021-11-28 12:12:15 -0500

    add functionality for 'true' and 'false' buttons
    when button is pressed, compare user answer to answer of question, call another question and populate text window with new question

--- ui.py
+++ ui.py
@@ -1,4 +1,6 @@
 from tkinter import *
+
+import quiz_brain
 from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
@@ -12,8 +14,8 @@ class QuizInterface:
         self.window = Tk()
         self.window.title('Quizzler')
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
-        self.score = 0
-        self.score_text = Label(text=f'Score: {self.score}', fg='white', bg=THEME_COLOR, font=SCORE_FONT)
+
+        self.score_text = Label(text=f'Score: 0', fg='white', bg=THEME_COLOR, font=SCORE_FONT)
         self.score_text.grid(row=0, column=1)
         self.quiz_text_window = Canvas(width=300, height=250, bg='white', highlightthickness=0)
         self.quiz_text_window.grid(row=1, column=0, columnspan=2, pady=40)
@@ -21,8 +23,10 @@ class QuizInterface:
                                                            fill='black', font=QUIZ_FONT)
         self.true_image = PhotoImage(file='images/true.png')
         self.false_image = PhotoImage(file='images/false.png')
-        self.true_button = Button(image=self.true_image, highlightthickness=0)
-        self.false_button = Button(image=self.false_image, highlightthickness=0)
+        self.true_button = Button(image=self.true_image, highlightthickness=0,
+                                  command=self.answer_true)
+        self.false_button = Button(image=self.false_image, highlightthickness=0,
+                                   command=self.answer_false)
         self.true_button.grid(row=2, column=0)
         self.false_button.grid(row=2, column=1)
         self.get_next_question()
@@ -31,3 +35,11 @@ class QuizInterface:
     def get_next_question(self):
         question = self.quiz.next_question()
         self.quiz_text_window.itemconfig(self.quiz_text, text=f'{question}')
+
+    def answer_true(self):
+        self.quiz.check_answer('True')
+        self.get_next_question()
+
+    def answer_false(self):
+        self.quiz.check_answer('False')
+        self.get_next_question()
